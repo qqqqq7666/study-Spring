49376a992355930b7bc24d314153fd3ba584e7d1
rudcjs <rudcjs335@gmail.com>
2025-09-14 19:54:18 +0900

    JwtUtil 추가 - 토큰 생성 및 검증

---
+++ src/main/java/kr/co/spartaspring/AuthController.java
@@ -0,0 +1,38 @@
+package kr.co.spartaspring;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.spartaspring.entity.UserRole;
+import kr.co.spartaspring.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AuthController {
+    private final JwtUtil jwtUtil;
+
+    @GetMapping("/create-jwt")
+    public String createJwt(HttpServletResponse response) {
+        String token = jwtUtil.createToken("nada", UserRole.USER);
+
+        jwtUtil.addJwtToCookie(token, response);
+        return "createJwt : " + token;
+    }
+
+    @GetMapping("/jwt")
+    public String getJwt(@CookieValue(JwtUtil.HEADER) String tokenValue) {
+        String token = jwtUtil.substringToken(tokenValue);
+
+        if(!jwtUtil.validateToken(token))
+            throw new IllegalArgumentException("Token Error");
+
+        Claims info = jwtUtil.getUserInformationFromToken(token);
+
+        return "getJwt : " + info.getSubject() + " | " + info.get(JwtUtil.KEY);
+    }
+}
--- src/main/java/kr/co/spartaspring/SpartaSpringApplication.java
+++ src/main/java/kr/co/spartaspring/SpartaSpringApplication.java
@@ -2,8 +2,9 @@ package kr.co.spartaspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpartaSpringApplication {
 
     public static void main(String[] args) {
