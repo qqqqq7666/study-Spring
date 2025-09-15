e4067bf6deffc3506bd9119c88b515f69a033270
rudcjs <rudcjs335@gmail.com>
2025-09-15 15:39:20 +0900

    init commit

---
+++ src/main/java/kr/co/spartaspring/naver/controller/NaverApiController.java
@@ -0,0 +1,11 @@
+package kr.co.spartaspring.naver.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class NaverApiController {
+}
